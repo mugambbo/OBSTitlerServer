8fce08dc2ad78a8c2b24ada969c7c583b328920c
Abdulmajid <abdulmajid@hellotractor.com>
2020-05-02 13:22:24 +0100

    1. Change default file location to home directory 2. Add gitignore file

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const port = 3000
 var cors = require('cors');
 const bodyParser  = require('body-parser');
 const fs = require('fs');
+const homedir = require('os').homedir();
 
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/', (req, res) => {
     const body = req.body;
-    fs.writeFile('current_title.txt', body.title, 'utf8', () => {
-        console.log("Body title saved: "+JSON.stringify(body));
+    fs.writeFile(`${homedir}/current_title.txt`, body.title, 'utf8', (err) => {
+        if (err) res.status(err.code).json(err.message);
+        console.log(`${homedir}/current_title.txt`);
+        console.log("Body title save: "+JSON.stringify(body));
         res.json({status: 200});
     });
 })
